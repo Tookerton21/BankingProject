476548963ed24d9f18f5489c0b996b72db323033
Tookerton21 <nicruiz@gmail.com>
2018-05-31 02:13:05 -0700

    second commit

--- BuildData.hs
+++ BuildData.hs
@@ -1,12 +1,29 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import           Control.Applicative
+import           Data.Int (Int64)
+import           Data.Text (Text)
 import qualified Data.Text as T
 import           Database.SQLite.Simple
 import           Database.SQLite.Simple.FromRow
 import Control.Monad
 
-data TestField = TestField Int T.Text T.Text deriving (Show)
+data User =
+  User
+  { id        :: Int64
+  , fname     :: Text
+  , lname     :: Text
+  , checking  :: Double
+  , savings   :: Double
+  } deriving (Eq, Read, Show)
+
+
+
+instance FromRow User where
+  fromRow = User <$> field <*> field <*> field <*> field <*> field
+
+instance ToRow User where
+  toRow (User id_ fName lName c s) = toRow(fName, lName, c, s)
 
 data UserField = UserField Int T.Text T.Text Int Int deriving (Show)
 instance FromRow UserField where
@@ -18,34 +35,10 @@ data Sav = Sav Integer deriving (Show)
 instance FromRow Sav where
   fromRow = Sav <$> field
 
--- instance ToRow Sav where
---  toRow (Sav savings) = toRow (savings)
+instance FromRow Int where
+  fromRow = field
 
-instance FromRow TestField where
-  fromRow = TestField <$> field <*> field  <*> field
 
-instance ToRow TestField where
-  toRow (TestField id_ str str2) = toRow (id_, str, str2)
-
-{-
-main :: IO ()
-main = do
-  conn <- open "test.db"
-  execute_ conn "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, str TEXT, str2 Text)"
-  let strf = "fname"::String
-  let strn = "lname"::String
-  execute conn "INSERT INTO test (str, str2) VALUES (?, ?)"
-               [strf, strn]
-  --execute conn "INSERT INTO test (id, str) VALUES (?,?)" (TestField 13 "test string 3")
-  --rowId <- lastInsertRowId conn
-  --executeNamed conn "UPDATE test SET str = :str WHERE id = :id" [":str" := ("updated str" :: T.Text), ":id" := rowId]
-  r <- query_ conn "SELECT * from test" :: IO [TestField]
-  mapM_ print r
-  -- execute conn "DELETE FROM test WHERE id = ?" (Only rowId)
-  close conn
--}
-
---  display UserData
 displayUser = do
   conn <- open "Bank.db"
   r <- query_ conn "SELECT * from users" :: IO[UserField]
@@ -65,43 +58,31 @@ addUser = do
 
   execute conn "INSERT INTO users (fName, lName, checking, savings) VALUES (?,?,?,?)"
                (fname, lname, c, s)
-
-  r <- query_ conn "SELECT * from users" :: IO [UserField]
-  mapM_ print r
+  close conn
+{-
+  Remove a user from the database base. Uses the usersId to delete them
+-}
+removeUser :: Int -> IO ()
+removeUser userId = do
+  conn <- open "Bank.db"
+  execute conn "DELETE FROM users WHERE id = ?" (Only userId)
   close conn
 
--- Removing a user from the database
-removeUser = do
-  conn <- open "Bank.db"
-
-  putStrLn "What user whould you like to remove"
-  putStrLn "Enter id: "
-  userId <- getLine
-
-  --r <- query_ conn "SELECT * from users" :: IO [TestField]
-  --mapM_ print r
-
-  execute conn "DELETE FROM users WHERE id = ?" (Only userId)
-
-  r <- query_ conn "SELECT * from users" :: IO [UserField]
-  mapM_ print r
-
-
- --modSavings amnt acntNum = do
-
 {-
-testQuery = do
-  conn <- open "Bank.db"
-  putStrLn "Enter User Id: "
-  userId <- getLine
-  r <- queryNamed conn "SELECT savings FROM users WHERE id = :id" [":id" := userId]
-  -- forM_ r $ \(savings)->
-  putStrLn "done"
+ Modify a users Savings with the parameter passed in amnt. Must pass the users Id
+ to make the modification.
 -}
---  xs <- query_ conn "select fName,savings from users"
---  forM_ xs $ \(fName,savings) ->
---    putStrLn $ T.unpack fName ++ " is " ++ show (savings :: Int)
-   --old <- execute conn "Select savings from users where id = ?" (Only acntNum)
-
---  putStrLn (read old)
-  -- execute conn "Modify"
+modSavings  :: Int -> Double -> IO ()
+modSavings userId amnt = do
+  conn <- open "Bank.db"
+  executeNamed conn "UPDATE users SET savings = :savings WHERE id = :id" [":savings" := amnt, ":id" := userId]
+  close conn
+{-
+  Modify a users checking to the parameter passed in as amnt. Must pass in the users
+  Id to make the modification
+-}
+modChecking  :: Int -> Double -> IO ()
+modChecking userId amnt = do
+  conn <- open "Bank.db"
+  executeNamed conn "UPDATE users SET checking = :checking WHERE id = :id" [":checking" := amnt, ":id" := userId]
+  close conn
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # BankingProject
+
+5/31/18
+I started cleaning up the project, and added two new deliverables of 
+moding the checking and savings to a value since the last push.
+
+functionality:
+- add a user to database
+- remove a user from database
+- set checking and savings
+- print function for testing/debugging 
+
+The next step is figuring out how to extract the current balance from both
+checking and savings. This way we can do the simple arithmetic to add and 
+subtract from the current balance.
+
+Known Bugs:
+- Have not figured out how to be able to use decimals. It lets you make the
+  modification to the database, but when I call the printUser functions I
+  get an Error. 
